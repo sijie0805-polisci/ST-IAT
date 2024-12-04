0fffea9cb50a708708ce8f2d1d822cbb972576de
sijie0805-polisci <sijie0805@gmail.com>
2024-12-03 22:29:37 -0500

    Update sample.js

--- sample.js
+++ sample.js
@@ -10,12 +10,12 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
 		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
+    		    	{word : 'Black people'}, 
+    			{word : 'Black people'}, 
+    			{word : 'Black people'}, 
+    			{word : 'Black people'}, 
+    			{word : 'Black people'}, 
+    			{word : 'Black people'}
 		    ], 
 		    //Stimulus css (style)
 		    css : {color:'#31b404','font-size':'3em'}
