daff4f32e85083eea72a4524192c62e2745841fd
sijie0805-polisci <sijie0805@gmail.com>
2024-12-05 15:50:42 -0500

    Update and rename sample7.js to sample8.js

--- sample8.js
+++ sample8.js
@@ -10,12 +10,12 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qua
 		      height : 7 //Used to position the "Or" in the combined block.
 		    }, 
 		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 'china1.jpg'}, 
-    			{image : 'china2.jpg'}, 
-    			{image : 'china3.jpg'}, 
-    			{image : 'china4.jpg'}, 
-    			{image : 'china5.jpg'}, 
-    			{image : 'china6.jpg'}
+    		    	{image : 'China1.jpg'}, 
+    			{image : 'China2.jpg'}, 
+    			{image : 'China3.jpg'}, 
+    			{image : 'China4.jpg'}, 
+    			{image : 'China5.jpg'}, 
+    			{image : 'China6.jpg'}
 		    ], 
 		    //Stimulus css (style)
 		    css : {color:'#31b404','font-size':'3em'}
